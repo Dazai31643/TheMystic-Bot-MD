eaa2037a41112a2186b83a606b2ca380680689ae
Dazai31643 <163316742+Dazai31643@users.noreply.github.com>
2024-03-26 11:50:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -544,7 +544,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `[ ⏳ ] Uptime 𝐷𝐴𝑍𝐴𝐼 𝐵𝛩𝑇 : ${uptime}`;
+  const bio = `𝐷𝐴𝑍𝐴𝐼-𝐵𝛩𝑇 [ ⏳ ] Uptime: ${uptime}`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
