83fb1d5c4832e929c87a2867d8fbafdfdf48e115
Dazai31643 <163316742+Dazai31643@users.noreply.github.com>
2024-04-01 04:16:37 +0200

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,27 +1,19 @@
-import _translate from "./_translate.js"
-const tradutor = _translate.plugins.gc_tagall
-// Para configurar o idioma, na raiz do projeto altere o arquivo config.json
-// Para configurar el idioma, en la raíz del proyecto, modifique el archivo config.json.
-// To set the language, in the root of the project, modify the config.json file.
-
-const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
-  if (usedPrefix == 'a' || usedPrefix == 'A') return;
-  if (!(isAdmin || isOwner)) {
-    global.dfail('admin', m, conn);
-    throw false;
-  }
-  const pesan = args.join` `;
-  const oi = `${tradutor.texto1[0]} ${pesan}`;
-  let teks = `${tradutor.texto1[1]}  ${oi}\n\n${tradutor.texto1[2]}\n`;
-  for (const mem of participants) {
-    teks += `┣➥ @${mem.id.split('@')[0]}\n`;
-  }
-  teks += `*└* 𝐁𝐲 𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
-  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
-};
-handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
-handler.tags = ['group'];
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
-handler.admin = true;
-handler.group = true;
-export default handler;
+let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}//غير دازاي
+let pesan = args.join` `
+let oi = `*الرساله:* ${pesan}`
+let teks = `*  * \n\n ${oi}\n\n*🌀┇الجروب :⇣*\n`
+for (let mem of participants) {
+teks += `*تفاعل ↫* @${mem.id.split('@')[0]}\n`}
+teks += `*𝐷𝐴𝑍𝐴𝐼-𝐵𝛩𝑇*\n\n*𝐷𝐴𝑍𝐴𝐼*`
+conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+}
+handler.help = ['tagall <mesaje>','invocar <mesaje>']
+handler.tags = ['group']
+handler.command = /^(منشن|invocar|invocacion|todos|invocación)$/i
+handler.admin = true
+handler.group = true
+export default handler
