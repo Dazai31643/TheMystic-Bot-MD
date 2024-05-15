1b6da0119db2fefa4e8cd1b9f3d86ce709d1e43c
Dazai31643 <163316742+Dazai31643@users.noreply.github.com>
2024-05-16 01:33:40 +0300

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,7 +1,7 @@
 let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
 if (!(isAdmin || isOwner)) {
 global.dfail('admin', m, conn)
-throw true
+throw false
 }//غير دازاي
 let pesan = args.join` `
 let oi = `*الرساله:* ${pesan}`
@@ -14,6 +14,6 @@ conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, 
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
 handler.tags = ['group']
 handler.command = /^(منشن|invocar|invocacion|todos|invocación)$/i
-handler.admin = true
+handler.admin = false
 handler.group = true
 export default handler
